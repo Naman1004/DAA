00ac6885840dd5b82577e33d3aaa978571bf1e28
Naman <163288557+Naman1004@users.noreply.github.com>
2024-08-30 02:11:43 +0530

    Update and rename Implementation of Bubble Sort.c to Implementation of Bubble Sort.cpp

---
+++ Implementation of Bubble Sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            // Compare adjacent elements
+            if (arr[j] > arr[j + 1]) {
+                // Swap if they are in the wrong order
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        // If no elements were swapped, the array is sorted
+        if (!swapped)
+            break;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unsorted array: ";
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
